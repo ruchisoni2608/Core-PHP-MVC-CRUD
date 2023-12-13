56f57730abf4b8030384570664ba3479173bfa2d
Ruchi soni <106802039+ruchisoni2608@users.noreply.github.com>
2023-12-13 12:49:14 +0530

    Create header.php

---
+++ views/layouts/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Product Management</title>
+    <!-- <link rel="stylesheet" href="../../public/assets/bootstrap/css/bootstrap.min.css"> -->
+    <!-- <link rel="stylesheet" href="../../public/assets/datatables/datatables.min.css"> -->
+    <!-- <script src="../../public/assets/jquery/jquery.min.js"></script>
+    <script src="../../public/assets/bootstrap/js/bootstrap.min.js"></script> -->
+    <!-- <script src="../../public/assets/datatables/datatables.min.js"></script> -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .topnav a {
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+      
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
